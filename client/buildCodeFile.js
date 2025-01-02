c8e50dacb2e3385970883793ed9fea4a5c4c6c33
Khang Hoang <hoangtrieukhang@gmail.com>
2025-01-01 23:34:53 -0800

    refactor

--- client/buildCodeFile.js
+++ client/buildCodeFile.js
@@ -1,30 +1,24 @@
 import fs from "node:fs/promises";
 
 // Get contents from code file in questionsBank/ and build the json file that contains codes at build time
-async function main() {
-  const res = [];
-  const path = "./questionsBank/python3/";
-  const files = await fs.readdir(path);
-  const tasks = [];
-  for (const file of files) {
-    const fileContent = fs.readFile(path + file, "utf-8").then((fileContent) => {
-      const question = [
-        {
-          id: file,
-          code: fileContent,
-        },
-      ];
-      res.push(question);
-    });
+const res = [];
+const path = "./questionsBank/python3/";
+const files = await fs.readdir(path);
+const tasks = [];
+for (const file of files) {
+  const fileContent = fs.readFile(path + file, "utf-8").then((fileContent) => {
+    const question = [
+      {
+        id: file,
+        code: fileContent,
+      },
+    ];
+    res.push(question);
+  });
 
-    tasks.push(fileContent);
-  }
-
-  await Promise.all(tasks);
-
-  await fs.writeFile("./src/pages/components/codefiles/pyCode.json", JSON.stringify(res));
+  tasks.push(fileContent);
 }
 
-(async () => {
-  await main();
-})();
+await Promise.all(tasks);
+
+await fs.writeFile("./src/pages/components/codefiles/pyCode.json", JSON.stringify(res));
--- client/buildCodeFiles.js
+++ client/buildCodeFiles.js
@@ -1,27 +1,20 @@
 import fs from "node:fs/promises";
 
-async function main() {
-  const path = "./src/pages/components/codefiles/pyCode.json";
-  const fileContent = await fs.readFile(path, "utf-8");
+const path = "./src/pages/components/codefiles/pyCode.json";
+const fileContent = await fs.readFile(path, "utf-8");
 
-  const questionsJSON = JSON.parse(fileContent);
-  const tasks = [];
+const questionsJSON = JSON.parse(fileContent);
+const tasks = [];
 
-  for (const questionWrapper of questionsJSON) {
-    if (questionWrapper == null) {
-      continue
-    }
-
-    const question = questionWrapper[0]
-
-    const makeQuestionFile = fs.writeFile(`./questionsBank/python3/${question.id}`, question.code)
-    tasks.push(makeQuestionFile)
+for (const questionWrapper of questionsJSON) {
+  if (questionWrapper == null) {
+    continue;
   }
 
-  await Promise.all(tasks);
+  const question = questionWrapper[0];
 
+  const makeQuestionFile = fs.writeFile(`./questionsBank/python3/${question.id}`, question.code);
+  tasks.push(makeQuestionFile);
 }
 
-(async () => {
-  await main();
-})();
+await Promise.all(tasks);
