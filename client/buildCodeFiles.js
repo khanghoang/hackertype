1717aefc7a96ef4e1db45826e9f440be6d80bb4b
Khang Hoang <hoangtrieukhang@gmail.com>
2025-01-01 22:34:34 -0800

    add scripts to build the code files and gen runtime codefile.json

---
+++ client/buildCodeFiles.js
@@ -0,0 +1,27 @@
+import fs from "node:fs/promises";
+
+async function main() {
+  const path = "./src/pages/components/codefiles/pyCode.json";
+  const fileContent = await fs.readFile(path, "utf-8");
+
+  const questionsJSON = JSON.parse(fileContent);
+  const tasks = [];
+
+  for (const questionWrapper of questionsJSON) {
+    if (questionWrapper == null) {
+      continue
+    }
+
+    const question = questionWrapper[0]
+
+    const makeQuestionFile = fs.writeFile(`./questionsBank/python3/${question.id}`, question.code)
+    tasks.push(makeQuestionFile)
+  }
+
+  await Promise.all(tasks);
+
+}
+
+(async () => {
+  await main();
+})();
